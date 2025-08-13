a4c3cb981f1cb5e27be1ac226c3b0e2ce1e8992b
user050507280221 <angelbenitezayado477@gmail.com>
2025-08-13 01:15:48 +0000

    ACT2

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    
+    <?php include 'main.php'; ?>
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,16 @@
+<?php
+$name = "BENITEZ, ANGEL A.";
+$age = "20";
+$school = "GRC";
+$course = "BSIT";
+$subject = " WEBSYS";
+$student = yes;
+
+echo "I am $name \n";
+echo "I am $age years old \n";
+echo "I am from $school \n";
+echo "My course is $course \n";
+echo "I am doing my $subject activity \n";
+echo "Are you Student? $student ";
+
+?>
